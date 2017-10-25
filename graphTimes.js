0913090bc74e53ce66c9841bfd6fd3ce88c6d93c
Scott Veale <scottveale@Scotts-MacBook-Pro.local>
2017-10-24 18:30:50 -0600

    refactored graphTimes to use promises

--- graphTimes.js
+++ graphTimes.js
@@ -2,19 +2,23 @@ const mongoose = require ('mongoose');
 const _ = require('lodash');
 const { Log } = require('./db/logModel');
 
-const hospital = "Medicine Hat Regional Hospital";
+const hospital = "Alberta Children's Hospital";
 
 const graphTimes = (hospital) => {
-	mongoose.connect('mongodb://localhost/ewt',  {}, async (err) => {
-		console.log('hospital', hospital)
-		const logs = await Log.find({ "name": hospital });
-		console.log('logs', logs.length);
-		// let minutes = Number(log.waitTime.split(':')[0])*60 + Number(log.waitTime.split(':')[1])
-		logs.map(log => {
-			const [hours, minutes] = log.waitTime.split(':');
-			const totalTime = Number(hours) * 60 + Number(minutes);
-			console.log(totalTime + ' ')
+	mongoose.connect('mongodb://localhost/ewt')
+	.then(()=> {
+		const logs = Log.find({ "name": hospital })
+		.then((logs) => {
+			logs.map(log => {
+				const [hours, minutes] = log.waitTime.split(':');
+				const totalTime = Number(hours) * 60 + Number(minutes);
+				console.log(totalTime + " ");
+			})
+		})
+		.then(() => {
+			mongoose.disconnect();
 		})
 	})
 }
-graphTimes(hospital);
+
+graphTimes(hospital)
